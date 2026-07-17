3121747f08961c9cbd61335ea59d0cb6f3875cc2
depahasinireddy <hasinireddydepa@gmail.com>
2026-07-17 15:21:10 +0530

    Force overwrite Week-5 with local copy

---
+++ Week-5/6. ReactJS-HOL/code/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Cognizant Trainers Portal</h2>
+      <p>Select "Trainers" to view all available trainers and their expertise.</p>
+    </div>
+  );
+}
+
+export default Home;
