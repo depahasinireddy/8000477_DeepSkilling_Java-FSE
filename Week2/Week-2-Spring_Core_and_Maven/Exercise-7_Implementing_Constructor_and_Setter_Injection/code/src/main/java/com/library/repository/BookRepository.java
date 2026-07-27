51146f471fcbfa48269e102d151c69c7f2a006e6
Depa Hasini <hasinireddydepa@gmail.com>
2026-07-27 11:05:29 +0530

    Update HandsOn files

--- Week2/Week-2-Spring_Core_and_Maven/Exercise-7_Implementing_Constructor_and_Setter_Injection/code/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void save(String bookTitle) {
-        System.out.println("Book '" + bookTitle + "' saved to repository.");
-    }
-}
--- Week2/Week-2-Spring_Core_and_Maven/Exercise-8_Implementing_Basic_AOP_with_Spring/code/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void store(String bookTitle) {
-        System.out.println("Repository: '" + bookTitle + "' saved.");
-    }
-}
